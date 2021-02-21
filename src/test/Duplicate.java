ebf97ef74163ef0163ebe2e06fc727858b6257c2
HemantFullStack <yadavh027@gmail.com>
2021-02-21 16:27:32 +0530

    here we go

--- src/test/Duplicate.java
+++ src/test/Duplicate.java
@@ -1,9 +1,35 @@
 package test;
 
 public class Duplicate {
+
+    int x;
+
     public static void main(String[] args) {
+
         System.out.println("new file ");
-        //testing here
-        //testing again 
+        Duplicate dd = new Duplicate(); // creating object
+
+        for(String s : args){
+            System.out.println(s);
+        }
+
+        dd.play1(3);
+        dd.play();
+        Duplicate.play2(2,1);
+        dd.x = 3;
+
+    }
+
+
+    private void play() {
+
+    }
+
+    private void play1(int x) {
+
+    }
+
+    private static void play2(int x,int y){
+
     }
 }
