5e04cf9b1dae8299fd3f1ea6145a6acd61062e4c
HemantFullStack <yadavh027@gmail.com>
2021-02-21 09:54:59 +0530

    First commit

---
+++ src/bike/Parts.java
@@ -0,0 +1,30 @@
+package bike;
+
+public class Parts {
+    public static void main (String[] args)
+    {
+        System.out.println("hello world");
+        int a=10;
+        float b=50f;
+        System.out.println(a);
+        System.out.println(a+10);
+        System.out.println(b);
+        if (a>b)
+        {
+            System.out.println("a is gtreater than b");
+        }
+        else
+        {
+            System.out.println("b is greater than a");
+        }
+
+        for(int i=0;i<3;i++)
+        {
+            System.out.println("*");
+        }
+
+    }
+
+
+}
+
